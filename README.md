3ba9b62fc9cf94c1e81aa41c216226cddf1a697d
Keith A. Lidke <kalidke@users.noreply.github.com>
2023-05-09 15:32:48 -0600

    doc updates

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
-# SMLMMetrics
+# SMLMMetrics.jl
+
+SMLMMetrics is a Julia package for analyzing single molecule super-resolution data. It provides metrics and tools to evaluate the quality of your super-resolution microscopy data, allowing you to compare different imaging techniques and processing methods.
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://JuliaSMLM.github.io/SMLMMetrics.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://JuliaSMLM.github.io/SMLMMetrics.jl/dev/)
 [![Build Status](https://github.com/JuliaSMLM/SMLMMetrics.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/JuliaSMLM/SMLMMetrics.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/JuliaSMLM/SMLMMetrics.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/JuliaSMLM/SMLMMetrics.jl)
+
+## Features
+
+- Jaccard Index calculation for comparing localization sets
+- Root Mean Square Error (RMSE) calculation for evaluating localization accuracy
+- Dimensional weighting for RMSE calculations
+- Flexible support for various data dimensions
+
+## Installation
+
+To install SMLMMetrics, use the Julia package manager:
+
+```julia
+using Pkg
+Pkg.add("SMLMMetrics")
+```
+
+## Documentation
+
+- [**STABLE**](https://JuliaSMLM.github.io/SMLMMetrics.jl/stable/) - Documentation for the most recently tagged version of SMLMMetrics.
+- [**DEVELOPMENT**](https://JuliaSMLM.github.io/SMLMMetrics.jl/dev/) - Documentation for the in-development version of SMLMMetrics.
+
+## Usage
+
+After installing the package, you can start using SMLMMetrics in your project:
+
+```julia
+using SMLMMetrics
+```
+
+For more details and examples, please refer to the [documentation](https://JuliaSMLM.github.io/SMLMMetrics.jl/stable/).
+
+## Contributing
+
+Contributions to SMLMMetrics are always welcome! If you have any suggestions, feature requests, or bug reports, please open an issue on [GitHub](https://github.com/JuliaSMLM/SMLMMetrics.jl/issues).
+
+## License
+
+SMLMMetrics.jl is licensed under the [MIT License](LICENSE).
--- src/jaccard.jl
+++ src/jaccard.jl
@@ -1,7 +1,7 @@
 # Jaccard Index function and methods
 
 """
-    jaccard(a,b,cutoff)
+    jaccard(a, b, cutoff)
 
 Calculate the Jaccard Index between the two sets `a` and `b` 
     using a maximum connection distance of `cutoff`.    
@@ -21,47 +21,48 @@ a cost matrix whose elements are:
   \\end{cases}``
 
 """
-function jaccard(a,b,cutoff)
+function jaccard(a, b, cutoff)
 end
 
 
 """
-    jaccard(a::Array{<:Real},b::Array{<:Real},cutoff::Vector[<:Real])
+    jaccard(a::Array{<:Real}, b::Array{<:Real}, cutoff::Vector{<:Real})
 
 `a` and `b` are `d` x `n`, and `d` x `m` arrays, where `d` is the number of dimensions  
 
 `cutoff` contains the maximum matching distance for each dimension.
 
 """
-function jaccard(a::Array{<:Real},b::Array{<:Real},cutoff::Vector{<:Real})
+function jaccard(a::Array{<:Real}, b::Array{<:Real}, cutoff::Vector{<:Real})
 
-    assignment = match(a,b,cutoff)
+    assignment = match(a, b, cutoff)
 
-    n_a = size(a,2)
-    n_b = size(b,2)
+    n_a = size(a, 2)
+    n_b = size(b, 2)
     n_match = sum(assignment .> 0.0)
     return (n_match) / (n_a + n_b - n_match)
 
 end
 
 """
-    function match(a::Array(<:Real),b::Array(<:Real), cutooff::Vector{<:Real})
+    function match(a::Array{<:Real}, b::Array{<:Real}, cutoff::Vector{<:Real})
 
 Find best match between points in `a` and `b`. 
+
 """
 function match(a::Array{<:Real}, b::Array{<:Real}, cutoff::Vector{<:Real})
     
-    n_a = size(a,2)
-    n_b = size(b,2)
-    n_d = size(a,1)
+    n_a = size(a, 2)
+    n_b = size(b, 2)
+    n_d = size(a, 1)
 
     largevalue = 1e6
-    costmatrix = largevalue.+zeros(Float32,n_a,n_b)
+    costmatrix = largevalue .+ zeros(Float32, n_a, n_b)
 
     # Pairwise distance for valid points 
     for i in 1:n_a, j in 1:n_b
-        if prod([abs(a[k,i]-b[k,j])<cutoff[k] for k in 1:n_d])
-            costmatrix[i,j] = sqrt(sum([abs2(a[k,i]-b[k,j]) for k in 1:n_d]))
+        if prod([abs(a[k, i] - b[k, j]) < cutoff[k] for k in 1:n_d])
+            costmatrix[i, j] = sqrt(sum([abs2(a[k, i] - b[k, j]) for k in 1:n_d]))
         end
     end
    
@@ -70,13 +71,12 @@ function match(a::Array{<:Real}, b::Array{<:Real}, cutoff::Vector{<:Real})
 
     # Unassign those with cost of largevalue
     for i in 1:lastindex(assignment)
-        if (assignment[i]>0) 
-            if costmatrix[i,assignment[i]]≈largevalue
-                assignment[i]=0
+        if (assignment[i] > 0) 
+            if costmatrix[i, assignment[i]] ≈ largevalue
+                assignment[i] = 0
             end
         end
     end
 
     return assignment
 end
-
