303fd10c0f319ecd21ce3cc2dca9ccdf1203f4f6
joel-283 <123031043+joel-283@users.noreply.github.com>
2023-11-17 13:33:47 -0700

    Changed to "Widen" rings
    This change does not solve issue with noise and results in seemingly falsely high correlations.

--- dev/frc_smlmvis.jl
+++ dev/frc_smlmvis.jl
@@ -1,14 +1,14 @@
 using GLMakie, Distributions, FFTW, Images, Noise, TestImages, SMLMData, SMLMSim, SMLMVis
+include("../src/SMLMMetrics.jl")
 
 #This are the default values for data simulation
 smld_true, smld_model, smld_noisy = SMLMSim.sim(;
-    ρ=1.0,
+    ρ=10.0,
     σ_PSF=0.13, #micron 
     minphotons=50,
-    ndatasets=10,
-    nframes=1000,
+    ndatasets=10,    nframes=1000,
     framerate=50.0, # 1/s
-    pattern=SMLMSim.NmerD(),
+    pattern=SMLMSim.Nmer2D(),
     molecule=SMLMSim.GenericFluor(; q=[0 50; 1e-2 0]), #1/s 
     camera=SMLMSim.IdealCamera(; xpixels=256, ypixels=256, pixelsize=0.1) #pixelsize is microns
 )
@@ -17,19 +17,32 @@ smld_true, smld_model, smld_noisy = SMLMSim.sim(;
 smld_noisy_1 = SMLMData.isolatesmld(smld_noisy, 1:Int(round(length(smld_noisy)/2)))
 smld_noisy_2 = SMLMData.isolatesmld(smld_noisy, Int(round(length(smld_noisy)/2)):Int(length(smld_noisy)))
 
-#img1 = SMLMVis.render_blobs(smld_noisy_1, normalization = :integral, n_sigmas = 3, colormap = :hot, zoom=1) 
+test_image_1, = SMLMVis.render_blobs(smld_noisy_1, zoom = 4)
+test_image_2, = SMLMVis.render_blobs(smld_noisy_2, zoom = 4)
 
-#Returns error, seems to be in creating a range of z values based on intensity when z and z_range are set to default values of nothing
+test_image_1 = parent(gray.(Gray.(test_image_1)))
+test_image_2 = parent(gray.(Gray.(test_image_2)))
 
-img1 = render_blobs(
-        (1, smld_noisy_1.datasize[2]),
-        (1, smld_noisy_1.datasize[1]),
-        smld_noisy_1.x,
-        smld_noisy_1.y,
-        smld_noisy_1.σ_x,
-        smld_noisy_1.σ_y;
-        normalization= :integral,
-        n_sigmas=3,
-        colormap=:hot,
-        zoom = 1
-    )
+radii, frc = SMLMMetrics.calcfrc(test_image_1, test_image_2, ringextension = 1)
+
+test_image_1_fft = fftshift(fft(test_image_1))
+test_image_2_fft = fftshift(fft(test_image_2))
+
+radii, frc = SMLMMetrics.calcfrc(test_image_1, test_image_2, ringextension = 1)
+
+fig = GLMakie.Figure(resolution=(1000,1300))
+
+
+image(fig[1,1], test_image_1, axis = (aspect = DataAspect(), title="Image 1"))
+image(fig[1,2], test_image_2, axis = (aspect = DataAspect(), title="Image 2"))
+
+ax3 = GLMakie.Axis(fig[2,1], title = "Magnitude of Fourier Transform Image 1", aspect=DataAspect())
+ax4 = GLMakie.Axis(fig[2,2], title = "Magnitude of Fourier Transform Image 2", aspect=DataAspect())
+
+heatmap!(ax3, abs.(test_image_1_fft))
+heatmap!(ax4, abs.(test_image_2_fft))
+
+ax5 = GLMakie.Axis(fig[3, 1:2], ylabel="FRC", xlabel = "Spatial Frequency (λ^-1)", title="FRC vs Spatial Frequency for Test Images")
+scatter!(ax5, radii, frc)
+hlines!(ax5, [1/7], linestyle = :dot)
+fig
--- src/frc.jl
+++ src/frc.jl
@@ -5,7 +5,7 @@ Calculate the Fourier Ring Correlation at a given radius (in pixels), given four
 
 Returns real part of the FRC
 """
-function corellationradius(img1::Matrix, img2::Matrix, radius, im_width, im_height)
+function corellationradius(img1::Matrix, img2::Matrix, im_width, im_height, radius)
 
     im_x_center = Int(floor(im_width/2))
     im_y_center = Int(floor(im_height/2))
@@ -33,6 +33,35 @@ function corellationradius(img1::Matrix, img2::Matrix, radius, im_width, im_heig
 end
 
 """
+Implementation with two specified Radii, inner and outer, increasding the width of the rings
+"""
+function correlationradius(img1::Matrix, img2::Matrix, im_width, im_height, inner_radius, outer_radius)
+    im_x_center = Int(floor(im_width/2))
+    im_y_center = Int(floor(im_height/2))
+
+    numerator_sum = 0
+    denominator_sum = 0
+
+    #Computes the Fourier ring correlation at a given magnitude of spatial frequency
+    for x in 1:im_width
+        for y in 1:im_height
+            if ((x - im_x_center)^2 + (y - im_y_center) ^2) >= inner_radius && ((x - im_x_center)^2 + (y - im_y_center) ^2) <= outer_radius
+                numerator_sum += (img1[y,x] * conj(img2[y,x]))
+                denominator_sum += sqrt(abs2(img1[y,x]) * abs2(img2[y,x]))
+            end
+        end
+    end
+
+    result = real(numerator_sum)/denominator_sum
+
+    if isnan(result)
+        @error "Ring not defined at R"
+        return NaN
+    end
+    return result
+end
+
+"""
 The FRC is a metric showing the correlation between two images as a function of spatial frequency.
 
 This method calculates the FRC of two greyscale images given the two images and the number of data points.
@@ -42,6 +71,7 @@ The number of datapoints determines the "size" of the rings as the resulting FRC
 The images are assumed to be square and the same size.
 
 """
+#=
 function calcfrc(img1::Matrix, img2::Matrix, numdatapoints::Int)
     im_width = size(img1)[2]
     im_height = size(img1)[1]
@@ -68,4 +98,40 @@ function calcfrc(img1::Matrix, img2::Matrix, numdatapoints::Int)
 
     return rounded_frc
 end
+=#
 
+"""
+Calculate the FRC with the option to increase 
+"""
+function calcfrc(img1::Matrix, img2::Matrix; ringextension::Int = 0)
+    im_width = size(img1)[2]
+    im_height = size(img1)[1]
+    
+    if size(img1) != size(img2) || size(img1)[1] != size(img1)[2]
+        @error "Images must be the same size and square"
+        return NaN
+    end
+    
+    if ringextension > Int(floor(im_width/8)) || ringextension < 0
+        @error "Ring size must be non-negative and less than or equal to the 1/8th the width of the image"
+        return NaN
+    end
+
+    #This array will contain all valid points for the radius
+    validradii = [i for  i in ringextension + 1: Int(floor(im_width/2) - ringextension)]
+
+    #Calculate the fourier transforms of the images, and shift them so that the zero frequency is in the center
+    img1_fft = fftshift(fft(img1))
+    img2_fft = fftshift(fft(img2))
+
+    #Now we will perform the FRC at each valid radius with an "extended" ring
+    frc = zeros(size(validradii))
+
+    for i in eachindex(validradii)
+        innerrad = validradii[i] - ringextension
+        outerrad = validradii[i] + ringextension
+
+        frc[i] = correlationradius(img1_fft, img2_fft, im_width, im_height, innerrad, outerrad)
+    end
+    return (validradii, frc)
+end
